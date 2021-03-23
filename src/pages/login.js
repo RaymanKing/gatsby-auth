5a3ab2c4ab57b37452f9bc0704a4aec6d30f5e26
RaymanKing <enrikerapela@gmail.com>
2021-03-23 09:43:26 +0100

    Functional

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,28 +1,57 @@
-import React from "react"
+import React, {useState, useContext} from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import firebase from 'gatsby-plugin-firebase'
+import { AuthContext } from '../context/auth'
+import { navigate } from "gatsby"
 
 const Login = () => {
+    const [data, setData] = useState({
+        email: "",
+        password: "",
+        error: null,
+    })
+
+    const { setUser } = useContext(AuthContext)
+
+    const handleChange = e => {
+        setData({ ...data, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+        setData({ ...data, error: null })
+        try{
+            const result = await firebase
+            .auth()
+            .signInWithEmailAndPassword(data.email, data.password)
+            setUser(result)
+            navigate("/")
+        } catch(err){
+            setData({ ...data, error: err.message })
+        }
+    }
+
     return (
         <Layout>
             <SEO title="Login" />
             <h4>Login</h4>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="email">Email</label>
                     <br />
-                    <input type="text" name="email" />
+                    <input type="text" name="email" value={data.email} onChange={handleChange} />
                     <br />
                     <br />
                 </div>
                 <div>
                     <label htmlFor="password">Password</label>
                     <br />
-                    <input type="password" name="password" />
+                    <input type="password" name="password" value={data.password} onChange={handleChange} />
                     <br />
                     <br />
                 </div>
-
+                {data.error ? <p style={{ color: "red"}}>{data.error}</p> : null}
                 <input type="submit" value="Login" />
             </form>
         </Layout>
